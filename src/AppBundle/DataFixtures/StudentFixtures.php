872683ce79a380bd0188bded0d71ea81c3cfdbc0
tribouillard <clement.tribouillard@econocom.com>
2017-12-16 10:56:25 +0100

    add Student fixtures

--- src/AppBundle/DataFixtures/StudentFixtures.php
+++ src/AppBundle/DataFixtures/StudentFixtures.php
@@ -5,21 +5,11 @@ namespace AppBundle\DataFixtures;
 use AppBundle\Entity\Student;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class StudentFixtures extends Fixture
 {
 
     private $cities = ["Paris", "Lyon", "Bordeaux", "Rennes", "Marseille"];
-
-    /**
-     * StudentFixtures constructor.
-     * @param UserPasswordEncoderInterface $encoder
-     */
-    public function __construct(UserPasswordEncoderInterface $encoder)
-    {
-        $this->encoder = $encoder;
-    }
 
     /**
      * @param ObjectManager $manager
@@ -32,8 +22,8 @@ class StudentFixtures extends Fixture
             $this->prepare("Pierre", "David", "54 rue du Pérou"),
             $this->prepare("Quetier", "Sylvie", "424 rue de grand champ"),
             $this->prepare("Courtemanche", "Lea", "759 boulevard de la République"),
-            $this->prepare("Quetier", "firstname" => "Christophe", "22 rue Napoléon"),
-            $this->prepare("lastname" => "Rojas", "firstname" => "Johanna", "address1" => "80 rue du 14 juillet")
+            $this->prepare("Quetier", "Christophe", "22 rue Napoléon"),
+            $this->prepare("Rojas", "Johanna", "80 rue du 14 juillet")
         );
 
         foreach ($students as $student) {
@@ -41,7 +31,7 @@ class StudentFixtures extends Fixture
         }
         $manager->flush();
 
-        }
+    }
 
     /**
      * @param $lastname
@@ -55,11 +45,13 @@ class StudentFixtures extends Fixture
         $student->setLastname($lastname);
         $student->setFirstname($firstname);
         $student->setAddress1($address1);
+        $student->setAddress2("");
         $student->setPostcode(mt_rand(10000, 99999));
         $student->setCity($this->cities[mt_rand(0, count($this->cities) - 1)]);
         $student->setPhone("06" . mt_rand(10000000, 99999999));
         $student->setEmail("{$firstname}.{$lastname}@yopmail.com");
-        $student->setPassword($this->encoder->encodePassword($student, 'pass_1234'));
+        $student->setPassword("pass_1234");
         return $student;
     }
+
 }
